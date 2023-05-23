ed018e8f4aac19fb447d9ff3315cebfc66cb0053
twegestal <96296150+twegestal@users.noreply.github.com>
2023-05-23 15:49:31 +0200

    set seller on add product

--- src/main/java/request/AddProductRequest.java
+++ src/main/java/request/AddProductRequest.java
@@ -5,11 +5,14 @@ import application.ProductManager;
 import beans.Product;
 import com.google.gson.Gson;
 import io.javalin.http.Context;
+import security.Security;
 
 public class AddProductRequest extends SafeRequest {
     @Override
     protected void handle(Context ctx) {
         Product product = new Gson().fromJson(ctx.body(), Product.class);
+        String seller = Security.extractUsernameFromToken(ctx);
+        product.setSeller(seller);
         Distributer.getInstance().getProductManager().addNewProduct(product);
         ctx.status(201);
     }
