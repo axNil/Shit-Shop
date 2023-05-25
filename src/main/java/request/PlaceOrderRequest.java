a7187667aec8e3840cbef988719d53a99421002f
Joel Eriksson Sinclair <joeles76@gmail.com>
2023-05-25 10:07:30 +0200

    Place Order request now handles the entire list of products, instead of only the last element.

--- src/main/java/request/PlaceOrderRequest.java
+++ src/main/java/request/PlaceOrderRequest.java
@@ -5,7 +5,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import io.javalin.http.Context;
-import request.utils.ResponseMessageBuilder;
 import security.Security;
 
 public class PlaceOrderRequest extends SafeRequest {
@@ -13,18 +12,26 @@ public class PlaceOrderRequest extends SafeRequest {
     protected void handle(Context ctx) {
         String username = Security.extractUsernameFromToken(ctx);
         JsonArray array = new Gson().fromJson(ctx.body(), JsonArray.class);
-        int productID = -1;
+
+        int okOrders = 0;
+        int productID;
         for (JsonElement e : array) {
             productID = e.getAsInt();
-        }
 
-        if (productID > -1) {
-            //order created
-            if (Distributer.getInstance().getOrderManager().placeOrder(username, productID)) {
-                ctx.status(201).result("Order placed.");
-                return;
+            if (productID > -1) {
+                //order created
+                if (Distributer.getInstance().getOrderManager().placeOrder(username, productID)) {
+                    okOrders++;
+                }
             }
         }
-        ctx.status(400).result("Nope.");
+
+        if(okOrders == array.size()) {
+            ctx.status(201).result("Order placed.");
+        } else if(okOrders == 0) {
+            ctx.status(400).result("No orders placed.");
+        } else {
+            ctx.status(201).result("Order partially placed.");
+        }
     }
 }
